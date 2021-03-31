61e2f4317c703f6f4f57d2ce1731e4b3dd89906b
Matt Vegas <matt@reactlabs.io>
2021-03-31 00:08:11 -0600

    add icons to the list

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class RandomWordsWidget extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWordsWidget> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   Widget _buildSuggestions() {
@@ -38,11 +39,16 @@ class RandomWordsState extends State<RandomWordsWidget> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
         title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ));
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ));
   }
 
   @override
