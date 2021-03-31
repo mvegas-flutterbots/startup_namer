d739ff272b90bc654d537e0484c5a6cf8076fc0f
Matt Vegas <matt@reactlabs.io>
2021-03-31 00:26:47 -0600

    navigate to a new screen for saved suggestions

--- lib/main.dart
+++ lib/main.dart
@@ -60,14 +60,37 @@ class RandomWordsState extends State<RandomWordsWidget> {
         });
   }
 
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
+        return ListTile(
+            title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ));
+      });
+      final List<Widget> divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved Suggestions'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     // final WordPair wordPair = WordPair.random();
     // return Text(wordPair.asPascalCase);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
+      appBar: AppBar(title: Text('Startup Name Generator'), actions: <Widget>[
+        IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+      ]),
       body: _buildSuggestions(),
     );
   }
